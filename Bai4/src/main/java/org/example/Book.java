1dafa2d5786eb67dec2c8ac3a38ed88938fdfc90
kaitohaidang <137318767+kaitohaidang@users.noreply.github.com>
2026-05-05 19:02:22 +0700

    add

---
+++ Bai4/src/main/java/org/example/Book.java
@@ -0,0 +1,15 @@
+package org.example;
+
+public class Book {
+    // Thuộc tính
+    public String title;
+    public String author;
+    public double price;
+
+    // Phương thức in thông tin
+    public void printInfo() {
+        System.out.println("Title: " + title);
+        System.out.println("Author: " + author);
+        System.out.println("Price: " + price);
+    }
+}
